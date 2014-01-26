d503660b114392f82d9da20ad6e1d4596fa06c72
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-01-26 11:55:46 -0800

    switch personal mail to fastmail

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,13 +1,16 @@
 import os
 
-def get_imap_passwd():
-  #cmd = "/Applications/MacPorts/CocoaDialog.app/Contents/MacOS/CocoaDialog secure-standard-inputbox --string-output --title 'OfflineIMAP: please input your password'"
-  #return os.popen(cmd).readlines()[1][:-1]
-
-  cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -a gmail -g"
+def get_keychain_password(key):
+  cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -a %s -g" % key
   line = os.popen(cmd).readline()
   passwds = line.split()
   if len(passwds) == 2:
     return passwds[1][1:-1]
   else:
     return ""
+
+def get_imap_password():
+    return get_keychain_password("fastmail-email")
+
+def get_imap_email():
+    return get_keychain_password("fastmail-username") + "@fastmail.fm"
