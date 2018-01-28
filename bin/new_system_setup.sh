d323f1fd207ce06e1d49da2c2bd9783a9983264e
Gary Bernhardt <gary.bernhardt@gmail.com>
2018-01-28 14:47:13 -0800

    update new system setup packages

--- bin/new_system_setup.sh
+++ bin/new_system_setup.sh
@@ -3,9 +3,8 @@
 set -e
 brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
 
-brew install git zsh vim ruby-install tmux wget fswatch ack postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby gem_home
+brew install git zsh vim ruby-install tmux wget fswatch ack postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby gem_home jpeg
 brew install imagemagick --with-ghostscript
 brew install ffmpeg --with-fdk-aac --with-x265
-brew install mecab mecab-ipadic
 
 ruby-install ruby 2.3.1
