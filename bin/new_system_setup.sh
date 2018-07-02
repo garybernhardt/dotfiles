7f1cbc41b266518a1cbc80b7679a198f3b4d2b78
Gary Bernhardt <gary.bernhardt@gmail.com>
2018-07-02 12:08:45 -0700

    switch from ack to ag

--- bin/new_system_setup.sh
+++ bin/new_system_setup.sh
@@ -3,7 +3,7 @@
 set -e
 brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
 
-brew install git zsh vim ruby-install tmux wget fswatch ack postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby gem_home jpeg
+brew install git zsh vim ruby-install tmux wget fswatch ag postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby gem_home jpeg
 brew install imagemagick --with-ghostscript
 brew install ffmpeg --with-fdk-aac --with-x265
 
