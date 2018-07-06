d980ec8396b348fef7d70b9c628960b27059cae2
Gary Bernhardt <gary.bernhardt@gmail.com>
2018-07-05 17:13:29 -0700

    update new_system_setup.sh for new mac

--- bin/new_system_setup.sh
+++ bin/new_system_setup.sh
@@ -3,8 +3,9 @@
 set -e
 brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
 
-brew install git zsh vim ruby-install tmux wget fswatch ag postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby gem_home jpeg
+brew install git zsh vim ruby-install tmux wget fswatch ag postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby jpeg selecta par2
+brew install --HEAD https://raw.github.com/postmodern/gem_home/master/homebrew/gem_home.rb
 brew install imagemagick --with-ghostscript
 brew install ffmpeg --with-fdk-aac --with-x265
 
-ruby-install ruby 2.3.1
+ruby-install ruby 2.4.2
