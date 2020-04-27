749346567ea2f7b20141ad2ac9caa1d950c6da82
Gary Bernhardt <gary.bernhardt@gmail.com>
2020-04-27 15:18:34 -0700

    update new_system_setup script for new mbp setup

--- bin/new_system_setup.sh
+++ bin/new_system_setup.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
-brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
+brew install reattach-to-user-namespace
 
-brew install git zsh vim ruby-install tmux wget fswatch ag postgresql lame python3 pngquant phantomjs heroku ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby jpeg selecta par2 tokei diff-so-fancy dust tig fd
-brew cask install skim
+brew install git zsh vim ruby-install tmux wget fswatch ag postgresql lame python3 pngquant ghc multimarkdown graphviz offlineimap tree lua colordiff hub chruby jpeg selecta par2 tokei diff-so-fancy dust tig fd nvm
 brew install --HEAD https://raw.github.com/postmodern/gem_home/master/homebrew/gem_home.rb
-brew install imagemagick --with-ghostscript
-brew install ffmpeg --with-fdk-aac --with-x265
+brew install imagemagick
+brew install ffmpeg
 
-ruby-install ruby 2.4.2
+# Don't animate windows opening. This is different from applications opening.
+# This applies when, for example, you have a running instance of Chrome or
+# TextEdit and hit Cmd-N.
+#
+# Last verified working: 2020-04-27
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
